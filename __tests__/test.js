f4b8809b56371171560fe15aeffedd3979002b00
Jon Horner <hornerjn@mail.uc.edu>
2015-03-17 11:09:01 -0400

    configured for jest testing

---
+++ __tests__/test.js
@@ -0,0 +1,30 @@
+// var chai = require('chai');
+// var assert = chai.assert;
+//
+// var config = require('../config.js');
+//
+// var BlackLion = require('./build/BlackLion.js');
+// var PSConnector = BlackLion.Connector;
+// var profileActions = BlackLion.ProfileActions;
+// var profileStore = BlackLion.ProfileStore;
+
+// describe('BlackLion', function() {
+//   describe('Profile', function() {
+//     it('should return `ok`', function(done) {
+//       var profileParams = {
+//         url: JSON.stringify(config.get('getProfileUrl')),
+//         acceptType: 'application/xml',
+//         headers: {
+//           Authorization: 'Basic aG9ybmVyam46UGFzc3dvcmQx'
+//         }
+//       };
+//
+//       profileActions.load(PSConnector, profileParams);
+//       profileStore.listen(function(profile) {
+//         console.log(JSON.stringify(profile));
+//         done();
+//       });
+//     });
+//   });
+// });
+console.log(window);
